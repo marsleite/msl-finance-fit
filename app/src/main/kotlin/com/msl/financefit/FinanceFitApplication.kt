1bd0b6177b77a5270bf9b1a65bbcf72ffcca1d9e
Marcelo <marcelo.leite@mercadolivre.com>
2023-08-06 15:20:04 -0300

    [feat] config environment

--- app/src/main/kotlin/com/msl/financefit/FinanceFitApplication.kt
+++ app/src/main/kotlin/com/msl/financefit/FinanceFitApplication.kt
@@ -1,5 +1,6 @@
 package com.msl.financefit
 
+import com.msl.financefit.application.config.setupEnv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,6 @@ import org.springframework.boot.runApplication
 class FinanceFitApplication
 
 fun main(args: Array<String>) {
+  setupEnv()
   runApplication<FinanceFitApplication>(*args)
 }
