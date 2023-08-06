27ef209d8731f24ce916fdaeb2cebacc34bd1ca4
Marcelo <marcelo.leite@mercadolivre.com>
2023-08-06 08:49:12 -0300

    [feat] scaffolding project

---
+++ src/main/kotlin/com/msl/financefit/FinanceFitApplication.kt
@@ -0,0 +1,11 @@
+package com.msl.financefit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceFitApplication
+
+fun main(args: Array<String>) {
+  runApplication<FinanceFitApplication>(*args)
+}
---
+++ src/test/kotlin/com/msl/financefit/FinanceFitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.msl.financefit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceFitApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
